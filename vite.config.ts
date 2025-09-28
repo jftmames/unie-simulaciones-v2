bdeba5982f87945200602c401e0c24db3e0f9629
Jose Fernández Tamame <jftamames@gmail.com>
2025-09-29 01:31:03 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  base: './'
-});
+// Si GH_PAGES=1, usamos base '/<REPO_NAME>/', si no, './' (Vercel)
+const base = process.env.GH_PAGES ? '/unie-simulaciones-v3/' : './';
+
+export default defineConfig({ base });
