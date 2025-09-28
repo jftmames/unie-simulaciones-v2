44ba41fe24b839caadc5dea02d60f7b9fae4f0a4
Jose Fernández Tamame <jftamames@gmail.com>
2025-09-29 01:41:28 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite';
 
-// Detecta si estamos en GitHub Pages (donde la URL lleva .github.io)
-const isGitHubPages = process.env.GITHUB_PAGES === 'true' || process.env.GH_PAGES === '1';
-
-// Nombre del repo (ajústalo si lo renombras en GitHub)
-const repoName = 'unie-simulaciones-v2';
+// Nombre EXACTO del repo en GitHub Pages
+const repo = 'unie-simulaciones-v2';
 
 export default defineConfig({
-  base: isGitHubPages ? `/${repoName}/` : './',
+  base: `/${repo}/`,   // requerido en GitHub Pages
 });
