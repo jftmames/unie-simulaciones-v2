c1b1a7cee4900134f7bc3eb2b608a3925862b519
Jose Fernández Tamame <jftamames@gmail.com>
2025-09-29 01:04:53 +0200

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <h2>Catálogo inicial</h2>
+  <ul>
+    <li><a href="#/fisica/leyes-newton">Física · Leyes de Newton</a></li>
+    <li><a href="#/matematicas/derivada">Matemáticas · Derivada</a></li>
+    <li><a href="#/informatica/logica-digital">Informática · Lógica digital</a></li>
+  </ul>
+`;
