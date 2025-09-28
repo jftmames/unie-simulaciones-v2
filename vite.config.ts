dc00be2133fcc31f4da7133e624538f98e31c9cc
Jose Fernández Tamame <jftamames@gmail.com>
2025-09-29 01:54:11 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'vite';
 export default defineConfig({
-  base: '/unie-simulaciones-v2/',  // nombre EXACTO del repo
+  base: '/unie-simulaciones-v2/', // nombre EXACTO del repo
 });
